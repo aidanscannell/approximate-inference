5a55222687622cedc964424502a16431cbdaec10
Aidan Scannell <as12528@my.bristol.ac.uk>
2019-01-14 01:15:41 +0000

    Added plot function for image segmentation

--- helpers.py
+++ helpers.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 def percent_correct(xImg, orgImg):
     """
@@ -49,4 +50,27 @@ def plot_iteration(xImg, i):
     ax = f.add_subplot(111)
     ax.imshow(xImg, cmap='gray')
     ax.set_title('De-noised Image (%d iterations)' % (i+1), fontsize=16)
+    plt.show()
+
+def plot(yImg, xImg):
+    maskF = np.full((xImg.shape[0], xImg.shape[1]), 0, dtype=np.uint8)  # mask is only
+    maskB = np.full((xImg.shape[0], xImg.shape[1]), 0, dtype=np.uint8)  # mask is only
+
+    maskF[xImg == 1] = 255
+    maskB[xImg == -1] = 255
+
+    maskF = cv2.bitwise_or(yImg, yImg, mask=maskF)
+    maskB = cv2.bitwise_or(yImg, yImg, mask=maskB)
+
+    f, axarr = plt.subplots(2, 2)
+    axarr[0, 0].imshow(yImg, cmap='gray')
+    axarr[0, 0].set_title('Original Image')
+    axarr[1, 0].imshow(xImg, cmap='gray')
+    axarr[1, 0].set_title('Latent Image')
+    axarr[0, 1].imshow(maskB, cmap='gray')
+    axarr[0, 1].set_title('Background Image')
+    axarr[1, 1].imshow(maskF, cmap='gray')
+    axarr[1, 1].set_title('Foreground Image')
+    plt.setp([a.get_xticklabels() for a in axarr[0, :]], visible=False) # Fine-tune figure; hide x ticks for top plots and y ticks for right plots
+    plt.setp([a.get_yticklabels() for a in axarr[:, 1]], visible=False)
     plt.show()
